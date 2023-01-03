cc8c98eb1aece99102216287e60fa7afbe7e4c8f
ichenny <ichenny.lee@gmail.com>
2023-01-03 14:45:00 +0900

    백준 1764번/23.01.03

---
+++ 이채은/1764번.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let [a, b] = input.shift().split(" ").map(Number);
+
+let nHear = new Set(input.slice(0, a));
+let nSee = new Set(input.slice(a));
+const common = [...nHear].filter((x) => nSee.has(x)).sort();
+
+console.log(`${common.length}\n${common.join("\n")}`);
