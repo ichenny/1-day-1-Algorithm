d934d27858c5317e2dfcb9198afc56c330d78350
94sssh <abizsh@naver.com>
2023-03-26 01:33:06 +0900

    백준 11279번/23.03.26

---
+++ 한승호/11279.js
@@ -0,0 +1,78 @@
+const input = require('fs').readFileSync('./index.txt').toString().trim().split('\n').map(Number);
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+  peek() {
+    return this.values[0];
+  }
+}
+
+let heap = new MaxBinaryHeap();
+let result = '';
+for (let i = 1; i <= input.length; i++) {
+  if (input[i] === 0) {
+    if (heap.values.length === 0) {
+      result += '0' + '\n';
+    } else {
+      result += heap.extractMax() + '\n';
+    }
+  } else {
+    heap.insert(input[i]);
+  }
+}
+console.log(result.slice(0, -1));
